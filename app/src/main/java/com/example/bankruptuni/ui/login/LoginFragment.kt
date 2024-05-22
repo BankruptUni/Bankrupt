5a0a5cd8e0ac05c38f2efaf98e40965c95ba2f2e
Thalles <thallesttt.11@gmail.com>
2024-05-22 20:32:28 -0300

    ajuste de criação dos logins

---
+++ app/src/main/java/com/example/bankruptuni/ui/login/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.example.bankruptuni.ui.login
+
+import androidx.fragment.app.Fragment
+import com.example.bankruptuni.databinding.FragmentLoginBinding
+
+class LoginFragment: Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+
+}
